921eaa972980bef88bfb82058a0d0f3ae3a3e8c7
WAsD-Ddos <mitja3375@gmail.com>
2025-04-11 23:12:21 +0300

    user_deposit + mistakes

--- Deposit.h
+++ Deposit.h
@@ -1,13 +1,13 @@
 #include "maint.h"
 
-
+#define K 365;
 
 class Deposit {
 
 public:
 	string name;
 	int age;
-	int deposit_years;
+	int deposit_days;
 	double deposit;
 	double persent;
 	bool attitude;
@@ -17,17 +17,17 @@ public:
 		//cout << "Default-construction " << endl;
 		name = "no name";
 		age = 18;
-		deposit_years = 1;
+		deposit_days = 1;
 		deposit = 0;
 		persent = 0;
 		attitude = false;
 	}
 	//canonical construction
-	Deposit(string user_name, int user_years,int user_deposit_years, double user_deposit, double interest, bool ratio) {
+	Deposit(string user_name, int user_years,int user_deposit_days, double user_deposit, double interest, bool ratio) {
 		//cout << "construction with argument " << endl;
 		name = user_name;
 		age = user_years;
-		deposit_years= user_deposit_years
+		deposit_days = user_deposit_days;
 		deposit = user_deposit;
 		persent = interest;
 		attitude = ratio;
@@ -43,12 +43,20 @@ public:
 	string toString() {
 		string res = "The user name: " + name + "\n";
 		res += "The age user: " + to_string(age) + "\n";
-		res += "The user deposit years: " + to_string(deposit_years) + "\n";
+		res += "The user deposit days: " + to_string(deposit_days) + "\n";
 		res += "The deposit: " + to_string(deposit) + "\n";
 		res += "The dpersent user: " + to_string(persent) + "\n";
 		res += "The attitude user: ";
 		res += attitude ? "good" : "no";
 		res += "\n";
+		return res;
+	}
+
+	double userDeposit(int user_deposit_days, double user_deposit, double interest) {
+
+
+		double res=(user_deposit * interest * user_deposit_days) / K;
+		res /= 100;
 		return res;
 	}
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "Deposit.h"
 
 int main() {
-	Deposit user1("Vlad", 19,3, 257.3, 7, true);;
+	Deposit user1("Vlad", 19, 273 , 350000 , 4.7 , true);//name | age | deposit days | deposit | persent | attitute
 	
 	cout << user1.toString() << endl;
+	cout << to_string(user1.userDeposit(user1.deposit_days, user1.deposit, user1.persent)) << endl;
 	return 0;
 }
